880294369dac845de4ba45e627724e66a32465f3
horja.robert <horja.robert@gmail.com>
2019-11-27 15:01:16 +0200

    Humidity/Temperature - Comfort

--- SmartHome/SWOutputs.c
+++ SmartHome/SWOutputs.c
@@ -1,6 +1,16 @@
 #include <inttypes.h>
 #include <stdio.h>
 
+void SWPrint(char *str)
+{
+    printf("%s\n", str);
+}
+
+void SWDPrint(double val)
+{
+    printf("%f\n", val);
+}
+
 void SWDigitalWrite(uint8_t pin, uint8_t val)
 {
     printf("Pin (%d) write value set to <%d>\n", pin, val);
--- SmartHome/SmartHome.c
+++ SmartHome/SmartHome.c
@@ -1,12 +1,14 @@
 #include <Arduino.h>
 #include "Logic.h"
 #include "Lights.h"
+#include "Comfort.h"
 
 
 void setup()
 {
     Serial.begin(9600);
     LightsInit();
+    ComfortInit();
 }
 
 void loop()
