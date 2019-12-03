fbcfaf36e21301723c6d33527c59dd4fa3f63ddd
horja.robert <horja.robert@gmail.com>
2019-12-03 17:18:20 +0200

    New commit | pushUp buttons

--- Comfort/ComfortTest/ComfortTest.ino
+++ Comfort/ComfortTest/ComfortTest.ino
@@ -1,20 +1,38 @@
 #include "dht.h"
-#define dht_apin A4 // Analog Pin 4
+#define dht_apin A4 // analog pin 4
+
+int buttonUp = 2; //digital pin 2
+int buttonUpState = 0;
+
+int buttonDown = 4; //digital pin 4
+int buttonDownState = 0;
+
+int incrementState = 0; //increment 1000 times this value...
+int delayMilliseconds = 1000; //delay for 'void loop'
+
+int previousStateButtonUp;
+int previousStateButtonDown;
 
 dht DHT;
+int desiredTemperature;
+int fahrenheitTemperature;
 
 uint8_t valueMQ135, valueMQ4;
 
 void setup()
 {
   Serial.begin(9600);
+
+  pinMode(buttonUp, INPUT);
+  pinMode(buttonDown, INPUT);
+
+  desiredTemperature = 25;
 }
 void loop()
 {
-  valueMQ135 = analogRead(A0);
-
   Serial.println("***loading***");
   //detect alcohol - MQ135
+  valueMQ135 = analogRead(A0);
   Serial.print("Alcohol level: ");
   Serial.print(valueMQ135);
   if (valueMQ135 > 220)
@@ -39,16 +57,84 @@ void loop()
   //humidity & temperature
   DHT.read11(dht_apin);
 
+
+  //read the state of the pushButton value
+  buttonUpState = digitalRead(buttonUp);
+  buttonDownState = digitalRead(buttonDown);
+
+  //after each 2 seconds...
+  if (incrementState % 2000  == 0) {
+    if (buttonDownState == HIGH) //&& previousStateButtonUp == 0)
+    {
+      DownTemperature();
+      //buttonDownState = LOW;
+    }
+    else if (buttonUpState == HIGH) //&& previousStateButtonDown == 0)
+    {
+      UpTemperature();
+      //buttonUpState = LOW;
+
+    }
+    else {
+      normalTemperature();
+    }
+  }
+
+  //previousStateButtonUp = buttonUpState;
+  //previousStateButtonDown = buttonDownState;
+  
+  Serial.println("\n");
+
+  delay(delayMilliseconds);
+  incrementState = incrementState + 1000;
+  if (incrementState == 10000) {
+    incrementState = 0;
+  }
+}
+
+int normalTemperature() {
   Serial.print("Humidity: ");
   Serial.print(DHT.humidity);
   Serial.print("%\n");
   Serial.print("Temperature: ");
   Serial.print(DHT.temperature);
   Serial.print("°C; ");
-
   Serial.print(DHT.temperature * 1.8 + 32);
   Serial.println("°F\n");
-  
-  Serial.println("\n");
-  delay(2000);
+}
+
+int UpTemperature() {
+
+  desiredTemperature++;
+  fahrenheitTemperature = (desiredTemperature * 1.8 + 32) + 1;
+
+  //maxim point
+  if (desiredTemperature >= 35 && fahrenheitTemperature >= 95) {
+    Serial.println("Maxim point! 35°C | 95°F");
+  }
+  else {
+    Serial.print("Set new Temperature: ");
+    Serial.print(desiredTemperature);
+    Serial.print("°C; ");
+    Serial.print(fahrenheitTemperature);
+    Serial.println("°F\n");
+  }
+}
+
+int DownTemperature() {
+
+  desiredTemperature--;
+  fahrenheitTemperature = (desiredTemperature * 1.8 + 32) - 1;
+
+  //minim point
+  if (desiredTemperature <= 9 && fahrenheitTemperature >= 48.2) {
+    Serial.println("Minim point! 9°C | 48.2°F");
+  }
+  else {
+    Serial.print("Set new Temperature: ");
+    Serial.print(desiredTemperature);
+    Serial.print("°C; ");
+    Serial.print(fahrenheitTemperature);
+    Serial.println("°F\n");
+  }
 }
